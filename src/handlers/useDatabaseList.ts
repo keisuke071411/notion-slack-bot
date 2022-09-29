b1415248908f6c321cc30dbfcc51c0a65ab2ec28
keisuke071411 <i.love.football0714@gmail.com>
2022-09-29 15:50:40 +0900

    Fix: Update custom get method and add error handle

--- src/handlers/useDatabaseList.ts
+++ src/handlers/useDatabaseList.ts
@@ -1,23 +1,32 @@
 import { Client } from "@notionhq/client";
 import { SayFn } from "@slack/bolt";
 
-export const useDatabaseList = async (hashTag: string, say: SayFn) => {
+export const useDatabaseList = async (tag: string, say: SayFn) => {
   try {
     const notion = new Client({ auth: process.env.NOTION_KEY })
+
+    if (!tag) throw new Error("No filter tag")
 
     const res = await notion.databases.query({
       database_id: process.env.DATABASE_ID as string,
       filter: {
         property: "Tags",
         multi_select: {
-          contains: "Event"
+          contains: tag
         }
       }
     })
 
-    say(JSON.stringify(res))
-  } catch (error) {
+    if (!res) throw new Error("unCatch notion List")
+
+    // @ts-ignore
+    const message = res.results.map((snapshot) => snapshot.properties.Name.title[0].text.content)
+    say(JSON.stringify(message))
+  } catch (error: unknown) {
     console.log(error)
     console.error('useDatabaseList func')
+    if (error instanceof Error) {
+      say(error.message)
+    }
   }
 }
