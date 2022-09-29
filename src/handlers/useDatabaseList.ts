90b4896a844f63b777c26a92331f23bd5338a324
keisuke071411 <i.love.football0714@gmail.com>
2022-09-29 17:38:16 +0900

    Add: createNotion method

--- src/handlers/useDatabaseList.ts
+++ src/handlers/useDatabaseList.ts
@@ -1,32 +1,65 @@
 import { Client } from "@notionhq/client";
 import { SayFn } from "@slack/bolt";
 
-export const useDatabaseList = async (tag: string, say: SayFn) => {
-  try {
-    const notion = new Client({ auth: process.env.NOTION_KEY })
+export const useDatabaseList = () => {
+  const notion = new Client({ auth: process.env.NOTION_KEY })
+  const databaseId = process.env.DATABASE_ID as string
 
-    if (!tag) throw new Error("No filter tag")
+  const getNotionList = async (tag: string, say: SayFn) => {
+    try {
 
-    const res = await notion.databases.query({
-      database_id: process.env.DATABASE_ID as string,
-      filter: {
-        property: "Tags",
-        multi_select: {
-          contains: tag
+      if (!tag) throw new Error("No filter tag")
+
+      const res = await notion.databases.query({
+        database_id: databaseId,
+        filter: {
+          property: "Tags",
+          multi_select: {
+            contains: tag
+          }
         }
+      })
+
+      if (!res) throw new Error("unCatch notion List")
+
+      // @ts-ignore
+      const message = res.results.map((snapshot) => snapshot.properties.Name.title[0].text.content)
+      say(JSON.stringify(message))
+    } catch (error: unknown) {
+      console.log(error)
+      console.error('useDatabaseList func')
+      if (error instanceof Error) {
+        say(error.message)
       }
-    })
-
-    if (!res) throw new Error("unCatch notion List")
-
-    // @ts-ignore
-    const message = res.results.map((snapshot) => snapshot.properties.Name.title[0].text.content)
-    say(JSON.stringify(message))
-  } catch (error: unknown) {
-    console.log(error)
-    console.error('useDatabaseList func')
-    if (error instanceof Error) {
-      say(error.message)
     }
   }
+
+  const createNotion = async (taskName: string, say: SayFn) => {
+    try {
+      const res = await notion.pages.create({
+        parent: { database_id: databaseId },
+        properties: {
+          Name: {
+            title: [
+              // @ts-ignore
+              {
+                "text": {
+                  "content": taskName
+                }
+              }
+            ]
+          }
+        },
+      })
+      say(JSON.stringify(res + "good"))
+    } catch (error: unknown) {
+      console.log(error)
+      console.error('useDatabaseList func')
+      if (error instanceof Error) {
+        say(error.message)
+      }
+    }
+  }
+
+  return { getNotionList, createNotion }
 }
--- src/server.ts
+++ src/server.ts
@@ -11,10 +11,24 @@ import { useDatabaseList } from './handlers/useDatabaseList'
 
 // 特定のハッシュタグのツイートを取得する(最大10件)
 app.command('/get_notion', async ({ body, ack, say }) => {
+  const { getNotionList } = useDatabaseList()
+
   try {
     await ack();
-    useDatabaseList(body.text, say)
+    getNotionList(body.text, say);
   } catch (error) {
-    console.log(error)
+    console.log(error);
+  }
+})
+
+app.command('/create_notion', async ({ body, ack, say }) => {
+  const { createNotion } = useDatabaseList()
+
+  try {
+    await ack();
+    say("good")
+    await createNotion(body.text, say);
+  } catch (error) {
+    console.log(error);
   }
 })
